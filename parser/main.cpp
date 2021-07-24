4806e672dcddc1dd21a3e12f1d61df26ab5f996b
Florentino Ngoc <vitmarkha@icloud.com>
2021-07-24 14:02:49 +0300

    first commit in parser

---
+++ parser/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc < 2 || argc > 3) {
+		cout << "There must be 2 or 3 arguments." << endl << "Example: [host:port_network:password_network] <port> <password>" << endl;
+	} else if (argc == 3) {
+
+	}
+	return 0;
+}
