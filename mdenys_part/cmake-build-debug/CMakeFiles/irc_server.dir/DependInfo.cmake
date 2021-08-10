2feeaf7daa4e87b580143548a98f1bd8d759e81d
Denis <denis.nadey@gmail.com>
2021-08-10 16:19:44 +0300

    add gitignore

---
+++ mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/lib/Channel/Channel.Class.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Channel/Channel.Class.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/lib/Executor/Executor.Class.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Executor/Executor.Class.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/lib/Server/Server.Class.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Server/Server.Class.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/lib/User/User.Class.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/lib/User/User.Class.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/lib/Utils/utils.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Utils/utils.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/mdenys_part/main.cpp" "/Users/denisnadey/projects/ft_irc/mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mdenys_part/cmake-build-debug/CMakeFiles/irc_server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/irc_server.dir/lib/Channel/Channel.Class.cpp.o"
+  "CMakeFiles/irc_server.dir/lib/Executor/Executor.Class.cpp.o"
+  "CMakeFiles/irc_server.dir/lib/Server/Server.Class.cpp.o"
+  "CMakeFiles/irc_server.dir/lib/User/User.Class.cpp.o"
+  "CMakeFiles/irc_server.dir/lib/Utils/utils.cpp.o"
+  "CMakeFiles/irc_server.dir/main.cpp.o"
+  "irc_server"
+  "irc_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/irc_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
