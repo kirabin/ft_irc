aebe88ae260a861a5d8050ab41b4cbaa19454a3c
VitMarKha <vitmarkha@icloud.com>
2021-08-18 17:29:49 +0300

    rename and test

---
+++ src/cmake-build-debug/CMakeFiles/irc_server.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vitmarkha/Desktop/ft_irc/src/lib/Channel/Channel.Class.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Channel/Channel.Class.cpp.o"
+  "/Users/vitmarkha/Desktop/ft_irc/src/lib/Executor/Executor.Class.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Executor/Executor.Class.cpp.o"
+  "/Users/vitmarkha/Desktop/ft_irc/src/lib/Server/Server.Class.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Server/Server.Class.cpp.o"
+  "/Users/vitmarkha/Desktop/ft_irc/src/lib/User/User.Class.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/lib/User/User.Class.cpp.o"
+  "/Users/vitmarkha/Desktop/ft_irc/src/lib/Utils/utils.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/lib/Utils/utils.cpp.o"
+  "/Users/vitmarkha/Desktop/ft_irc/src/main.cpp" "/Users/vitmarkha/Desktop/ft_irc/src/cmake-build-debug/CMakeFiles/irc_server.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
