95c07da782bc4b77fbc973e1f83a4356f5b8daeb
Denis <denis.nadey@gmail.com>
2021-08-28 15:05:23 +0300

    da

---
+++ cmake-build-debug/CMakeFiles/irc_server.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/denisnadey/projects/ft_irc/main.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/main.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Channel/Channel.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Channel/Channel.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Command.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Command.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/ErrorCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/ErrorCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/HelpCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/HelpCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/JoinCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/JoinCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/KickCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/KickCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/LeaveCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/LeaveCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/ListCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/ListCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/NickCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/NickCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/NoticeCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/NoticeCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/PassCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/PassCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/PrivateMessageCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/PrivateMessageCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/QuitCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/QuitCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/UserCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/UserCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Commands/WhoCommand.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Commands/WhoCommand.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Invoker/Invoker.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Invoker/Invoker.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Server/Server.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Server/Server.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/User/User.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/User/User.cpp.o"
+  "/Users/denisnadey/projects/ft_irc/src/Utils/utils.cpp" "/Users/denisnadey/projects/ft_irc/cmake-build-debug/CMakeFiles/irc_server.dir/src/Utils/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
