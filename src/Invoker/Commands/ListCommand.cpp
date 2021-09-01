7ac925f820bca013c0ae55a991c96035114c90fd
kirabin <kirabin.one@gmail.com>
2021-09-01 11:54:08 +0300

    Minor changes

--- src/Invoker/Commands/ListCommand.cpp
+++ src/Invoker/Commands/ListCommand.cpp
@@ -16,9 +16,9 @@ void ListCommand::execute() {
 	vector<Channel*> channels = _server->getChannels();
 	vector<Channel*>::iterator it;
 
-	_sender->getReply("Channels: " + std::to_string(channels.size()));
+	// _sender->getReply("Channels: " + std::to_string(channels.size()));
 
 	for (it = channels.begin(); it != channels.end(); it++) {
-		_sender->getReply((*it)->getName());
+		_sender->getReply("#" + (*it)->getName());
 	}
 }
